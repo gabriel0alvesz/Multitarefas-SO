ff62ccb0cb5c8e3b4777bf91a1310ff4f36eed17
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2022-09-18 22:12:09 -0300

    modularization functions ok, need to optimize

--- src/process.cpp
+++ src/process.cpp
@@ -38,7 +38,7 @@ void TokenizarInLine(string line_token, unordered_map<int, vector<string> > *ite
                token.append(",").append(to_string(count_column));
                (*itens)[count_line].push_back(token);
           
-          }
+          }else{(*classes)[token].push_back(count_line);}
 
           count_column++;
      }
@@ -82,12 +82,87 @@ void FileReadingT(unordered_map<int, vector<string> > *itensT,  unordered_map<st
                getline(process,line);
 
                TokenizarInLine(line,itensT,classesT,count_line);
-               count_line++;     
+               count_line++;
           }
 
      }else{cout << "erro" << endl;}
 
      process.close();
+}
+
+void CheckKeyValues(unordered_map<string, vector<int> > *itensD, unordered_map< int, vector<string> > *itensT, unordered_map< int, vector<string> > *newItens){
+
+     unordered_map<int, vector<string> > :: iterator it;
+     
+     for(it = itensT->begin(); it != itensT->end(); ++it){
+
+          for(string n: it->second){
+
+               if(itensD->find(n) != itensD->end()){(*newItens)[it->first].push_back(n);}
+          }
+     }
+
+}
+
+void Permutation(unordered_map<int, vector<string> > *newItens, unordered_map<int, vector<vector<string> > > *newItensPerm){
+
+     unordered_map<int, vector<string> > :: iterator it;
+     vector<string>::iterator itr, itrv, itrb;
+     
+     vector<string> vec_aux;
+     vector<vector<string> > vec_matrix;
+     
+     //Acessa cada chave dos itens
+     for(it = newItens->begin(); it != newItens->end(); ++it){
+
+          sort((*it).second.begin(), (*it).second.end()); // Custo O(n log(n)) -> ordena cada vetor para facilitar a etapa 3.
+
+          vec_matrix.clear();
+          for(itr = it->second.begin(); itr != it->second.end(); ++itr){
+               
+               vec_aux.clear();
+               vec_aux.push_back((*itr));
+               vec_matrix.push_back(vec_aux);
+          }
+
+          for(itr = it->second.begin(); itr != it->second.end(); ++itr){
+
+               itrv = itr;
+               ++itrv;
+               vec_aux.clear();
+               for(; itrv != it->second.end(); ++itrv){
+
+                    vec_aux.push_back(*itr);
+                    vec_aux.push_back(*itrv);
+                    vec_matrix.push_back(vec_aux);
+               }
+          }
+
+          for(itr = it->second.begin(); itr != it->second.end(); ++itr){
+
+               itrv = itr;
+               ++itrv;
+               vec_aux.clear();
+
+               for(; itrv != it->second.end(); ++itrv){
+                    
+                    itrb = itrv;
+                    ++itrb;
+                    vec_aux.clear();
+
+                    for(; itrb != it->second.end(); ++itrb){
+
+                         vec_aux.push_back(*itr);
+                         vec_aux.push_back(*itrv);
+                         vec_aux.push_back(*itrb);
+                         vec_matrix.push_back(vec_aux);
+
+                    }
+               }
+          }
+
+          (*newItensPerm)[it->first] = vec_matrix;
+     }
 }
 
 void ReadingFiles(){
@@ -97,11 +172,6 @@ void ReadingFiles(){
      
      unordered_map<string, vector<int> > itensD;
      unordered_map<string, vector<int> > classesD;
-
-     /**
-      * A impressão dos arquivos tokenizados é no formato:
-      * [chave,coluna] - linhas em que essa chave aparece nesta coluna. 
-      */
 
      FileReadingD(&itensD,&classesD);
      FileReadingT(&itensT,&classesT);
@@ -141,129 +211,23 @@ void ReadingFiles(){
      */
 
      //procurando se existe determiando valor na chave de D
-     int cont_aux = 0;
-     unordered_map<int, vector<string> > :: iterator it;
-     unordered_map<int, vector<string> > new_itens;
      
-     for(it = itensT.begin(); it != itensT.end(); ++it){
+     unordered_map<int, vector<string> > newItens;
+     
+     CheckKeyValues(&itensD,&itensT,&newItens);
 
-          for(string n: it->second){
-
-               if(itensD.find(n) != itensD.end()){
-
-                    //cout << "chave " << n << " encontrada!" << endl;
-                    new_itens[it->first].push_back(n);
-                    cont_aux++;
-               }
-          }
-     }
-
-     cout << cont_aux << endl;
-     PrintMap1(new_itens);
+     PrintMap1(newItens);
 
      cout << endl << endl;
 
      // Fazendo as permutacoes e armazenando em um novo Map
      // Percorrendo cada vetor com estes interadores.
      unordered_map<int, vector<vector<string> > > newItensPerm;
-     vector<string>::iterator itr, itrv, itrb;
      
-     vector<string> vec_aux;
-     vector<vector<string> > vec_matrix;
+     Permutation(&newItens, &newItensPerm);
+
+     PrintPermutation(&newItensPerm);
      
-     
-     //Acessa cada chave dos itens
-     for(it = new_itens.begin(); it != new_itens.end(); ++it){
-
-          //Ordena cada vetor de cada chave
-          sort((*it).second.begin(), (*it).second.end()); // Qual o custo?
-
-          //cout<< it->first << " :: ";
-
-          vec_matrix.clear();
-
-          for(itr = it->second.begin(); itr != it->second.end(); ++itr){
-               
-               vec_aux.clear();
-              vec_aux.push_back((*itr));
-              vec_matrix.push_back(vec_aux); 
-          }
-          
-     
-          for(itr = it->second.begin(); itr != it->second.end(); ++itr){
-
-               itrv = itr;
-               ++itrv;
-               vec_aux.clear();
-               for(; itrv != it->second.end(); ++ itrv){
-
-                    vec_aux.push_back(*itr);
-                    vec_aux.push_back(*itrv);
-                    vec_matrix.push_back(vec_aux);
-               }
-          }
-
-          for(itr = it->second.begin(); itr != it->second.end(); ++itr){
-
-               itrv = itr;
-               ++itrv;
-               vec_aux.clear();
-
-               for(; itrv != it->second.end(); ++itrv){
-                    
-                    itrb = itrv;
-                    ++itrb;
-                    vec_aux.clear();
-
-                    for(; itrb != it->second.end(); ++itrb){
-
-                         vec_aux.push_back(*itr);
-                         vec_aux.push_back(*itrv);
-                         vec_aux.push_back(*itrb);
-                         vec_matrix.push_back(vec_aux);
-
-                    }
-               }
-          }
-
-          //newItensPerm.insert( { it->first, vec_matrix } );
-          newItensPerm[it->first] = vec_matrix;
-         /*do{
-
-               if((*it).second.size() == 4){
-                    cout << *(itr+0) << " " << *(itr+1) << " " << *(itr+2) << " " << *(itr+3) << " # ";
-               }else if((*it).second.size() == 3){
-                    cout << *(itr+0) << " " << *(itr+1) << " " << *(itr+2) << " # ";
-               }else if((*it).second.size() == 2){
-                    cout << *(itr+0) << " " << *(itr+1) << " # d";
-               }
-          }while(next_permutation(itr, (*it).second.end()));
-          */
-
-          //cout << endl << endl;
-          
-     }
-     unordered_map<int, vector<vector<string> > >::iterator it_map;
-     
-     for(it_map = newItensPerm.begin(); it_map != newItensPerm.end(); ++it_map){
-
-          cout << it_map->first << " :: " << endl;;
-          
-          for(vector<string> n: it_map->second){
-
-               for(string a: n){
-
-                    cout << a << " ";
-               }
-               cout << endl;
-          }
-
-          cout << endl;
-     }
-
-
-     //cout << endl;
-     //PrintMap1(new_itens);
 }
 
 void PrintMap(unordered_map<string, vector<int> > map){
@@ -304,5 +268,21 @@ void PrintMap1(unordered_map<int, vector<string> > map){
      }
 }
 
+void PrintPermutation(unordered_map<int, vector<vector<string> > > *newItensPerm){
+
+     unordered_map<int, vector<vector<string> > >::iterator it_map;
+     
+     for(it_map = newItensPerm->begin(); it_map != newItensPerm->end(); ++it_map){
+
+          cout << it_map->first << " :: " << endl;;
+
+          for(vector<string> n: it_map->second){
+
+               for(string a: n){cout << a << " ";}
+               cout << endl;
+          }
+          cout << endl;
+     }
+}
 
 
--- src/process.hpp
+++ src/process.hpp
@@ -7,10 +7,9 @@
 #include <sstream>
 #include <vector>
 #include <unordered_map>
-#include <algorithm>
 
 using namespace std;
-    
+
 /**
  * @brief Faz a leitura do arquivo D.csv e T.csv - Realiza a etapa 1.
  */
@@ -18,8 +17,25 @@ void ReadingFiles();
 void FileReadingD(unordered_map< string, vector<int> > *itensD,  unordered_map< string, vector<int> > *classesD);
 void FileReadindT(unordered_map< int, vector<string> > *itensT,  unordered_map< string, vector<int> > *classesT);
 int Menu();
+
 /**
- * @brief Tokeniza linha a linha e separa em Hashs distintas
+ * @brief Faz a checagem dos valores do Map de ItensT com as keys do Map de itensD.
+ * 
+ * @param itensT Endereço do Map (&itensT).
+ * @param newItens Endereço do Map de receberá a chaves presentes em ambos (&newItens).
+ */
+void CheckKeyValues(unordered_map<int, vector<string> > *itensD,unordered_map< int, vector<string> > *itensT, unordered_map< int, vector<string> > *newItens);
+
+/**
+ * @brief Faz as permutações.
+ * 
+ * @param itensT Endereço do Map (&itensT).
+ * @param newItensPerm Endereço do Map quie armazenará as permutações (&newItensPerm).
+ */
+void Permutation(unordered_map<int, vector<string> > *newItens, unordered_map<int, vector<vector<string> > > *newItensPerm);
+
+/**
+ * @brief Tokeniza linha a linha e separa keys por coluna
  * 
  * @param line_token Linha a ser tokenizada
  * @param itens Endereço Hash de itens (&itens)
@@ -27,6 +43,15 @@ int Menu();
  * @param count_line contador de linhas
  */
 void TokenizarInColumn(string line_token, unordered_map<string, vector<int> > *itens,unordered_map<string, vector<int> > *classes, int count_line);
+
+/**
+ * @brief Tokeniza linha a linha e separa keys por linha
+ * 
+ * @param line_token Linha a ser tokenizada
+ * @param itens Endereço Hash de itens (&itens)
+ * @param classes Endereço Hash de classes (&classes)
+ * @param count_line contador de linhas
+ */
 void TokenizarInLine(string line_token, unordered_map<int, vector<string> > *itens,unordered_map<string, vector<int> > *classes, int count_line);
 
 /**
@@ -37,8 +62,13 @@ void TokenizarInLine(string line_token, unordered_map<int, vector<string> > *ite
 void PrintMap(unordered_map<string, vector<int> > map);
 void PrintMap1(unordered_map<int, vector<string> > map);
 
-// Para fazer o processamento basta verificar os valor de T com as chaves em D, as que estiverem nas chaves de D iram para um novo map
-// as permutacoes será para criar conjuntos e subconjutos dos elementos do novo map que estiverem.
+/**
+ * @brief Imprime Map com todas as permutações.
+ * 
+ * @param newItensPerm Endereço do Map de Permutações (&newItensPerm).
+ */
+void PrintPermutation(unordered_map<int, vector<vector<string> > > *newItensPerm);
+
 
 #endif
 
